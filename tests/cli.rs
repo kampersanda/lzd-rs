ca0b8cc038af0583d06f3515fedd88a4405e7b52
Shunsuke Kanda <shnsk.knd@gmail.com>
2021-07-18 04:34:27 +0900

    fix

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,8 @@
+#[cfg(feature = "cli_test")]
 use assert_cmd::Command;
 use std::fs::remove_file;
 
+#[cfg(feature = "cli_test")]
 fn do_test(file_name: &str) {
     let mut cmd = Command::cargo_bin("lzd").unwrap();
     cmd.arg(&file_name).arg("-f").arg("-t").assert().success();
@@ -8,73 +10,63 @@ fn do_test(file_name: &str) {
 }
 
 #[cfg(test)]
+#[cfg(feature = "cli_test")]
 mod tests {
     use super::*;
 
     const CANTRBRY_DIR: &str = "cantrbry";
 
     #[test]
-    #[ignore]
     fn alice29() {
         do_test(&format!("{}/alice29.txt", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn asyoulik() {
         do_test(&format!("{}/asyoulik.txt", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn cp() {
         do_test(&format!("{}/cp.html", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn fields() {
         do_test(&format!("{}/fields.c", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn grammar() {
         do_test(&format!("{}/grammar.lsp", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn kennedy() {
         do_test(&format!("{}/kennedy.xls", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn lcet10() {
         do_test(&format!("{}/lcet10.txt", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn plrabn12() {
         do_test(&format!("{}/plrabn12.txt", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn ptt5() {
         do_test(&format!("{}/ptt5", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn sum() {
         do_test(&format!("{}/sum", CANTRBRY_DIR));
     }
 
     #[test]
-    #[ignore]
     fn xargs() {
         do_test(&format!("{}/xargs.1", CANTRBRY_DIR));
     }
